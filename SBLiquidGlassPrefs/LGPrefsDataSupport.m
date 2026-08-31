ef78cc99668574f44025f3ca06e5be202cdee9e4
mowang7426 <972565726@qq.com>
2026-09-01 03:47:40 +0800

    Add files via upload

--- SBLiquidGlassPrefs/LGPrefsDataSupport.m
+++ SBLiquidGlassPrefs/LGPrefsDataSupport.m
@@ -66,24 +66,116 @@ static NSBundle *LGActiveLocalizationBundle(void) {
     return localizedBundle ?: baseBundle;
 }
 
-NSString *LGLocalized(NSString *key) {
-    return [LGActiveLocalizationBundle() localizedStringForKey:key value:key table:nil];
+static NSString *LGDisplayNameForLanguageCode(NSString *languageCode) {
+    if (!languageCode.length) return @"";
+    if ([languageCode isEqualToString:@"en"]) return @"English";
+
+    NSLocale *displayLocale = [NSLocale currentLocale];
+    NSString *localeIdentifier = [NSLocale canonicalLocaleIdentifierFromString:languageCode];
+    NSString *name = [displayLocale displayNameForKey:NSLocaleIdentifier value:localeIdentifier];
+    if (!name.length) {
+        NSDictionary *components = [NSLocale componentsFromLocaleIdentifier:localeIdentifier];
+        NSString *baseLanguageCode = components[NSLocaleLanguageCode];
+        if (baseLanguageCode.length) {
+            name = [displayLocale localizedStringForLanguageCode:baseLanguageCode];
+        }
+    }
+    return name.length ? name : languageCode;
 }
 
-static NSString *sLGLastSurfaceIdentifier = nil;
+static NSArray<NSDictionary *> *LGAvailableLanguageChoices(void) {
+    static NSArray<NSDictionary *> *choices;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSBundle *baseBundle = [NSBundle bundleForClass:[LGPRootListController class]];
+        NSMutableOrderedSet<NSString *> *codes = [NSMutableOrderedSet orderedSetWithObject:@"en"];
+        for (NSString *path in [baseBundle pathsForResourcesOfType:@"lproj" inDirectory:nil]) {
+            NSString *languageCode = [[path lastPathComponent] stringByDeletingPathExtension];
+            if (languageCode.length && ![languageCode isEqualToString:@"Base"]) {
+                [codes addObject:languageCode];
+            }
+        }
+
+        NSMutableArray<NSDictionary *> *dynamicChoices = [NSMutableArray arrayWithCapacity:codes.count];
+        for (NSString *languageCode in codes) {
+            [dynamicChoices addObject:@{
+                @"value": languageCode,
+                @"title": LGDisplayNameForLanguageCode(languageCode)
+            }];
+        }
+
+        [dynamicChoices sortUsingComparator:^NSComparisonResult(NSDictionary *lhs, NSDictionary *rhs) {
+            NSString *leftValue = lhs[@"value"];
+            NSString *rightValue = rhs[@"value"];
+            if ([leftValue isEqualToString:@"en"]) return NSOrderedAscending;
+            if ([rightValue isEqualToString:@"en"]) return NSOrderedDescending;
+            return [lhs[@"title"] localizedCaseInsensitiveCompare:rhs[@"title"]];
+        }];
+        choices = [dynamicChoices copy];
+    });
+    return choices;
+}
+
+Class LGPrefsSwitchClass(void) {
+    return NSClassFromString(@"LGPrefsLiquidSwitch") ?: [UISwitch class];
+}
+
+Class LGPrefsSliderClass(void) {
+    return NSClassFromString(@"LGPrefsLiquidSlider") ?: [UISlider class];
+}
+
+NSUserDefaults *LGPrefsUIStateDefaults(void) {
+    return [NSUserDefaults standardUserDefaults];
+}
+
+void LGSynchronizeSurfaceStateDefaults(void) {
+    [LGPrefsUIStateDefaults() synchronize];
+}
 
 NSString *LGLastSurfaceIdentifier(void) {
-    return sLGLastSurfaceIdentifier;
+    return [LGPrefsUIStateDefaults() stringForKey:kLGLastSurfaceKey];
 }
 
 void LGSetLastSurfaceIdentifier(NSString *identifier) {
-    sLGLastSurfaceIdentifier = [identifier copy];
+    NSUserDefaults *defaults = LGPrefsUIStateDefaults();
+    if (identifier.length) {
+        [defaults setObject:identifier forKey:kLGLastSurfaceKey];
+    } else {
+        [defaults removeObjectForKey:kLGLastSurfaceKey];
+    }
+    LGSynchronizeSurfaceStateDefaults();
 }
 
 void LGClearLastSurfaceIdentifierIfMatching(NSString *identifier) {
-    if ([sLGLastSurfaceIdentifier isEqualToString:identifier]) {
-        sLGLastSurfaceIdentifier = nil;
+    if (!identifier.length) return;
+    NSString *current = LGLastSurfaceIdentifier();
+    if ([current isEqualToString:identifier]) {
+        LGSetLastSurfaceIdentifier(nil);
     }
+}
+
+void LGObservePrefsNotifications(id target) {
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:target
+               selector:@selector(handlePrefsUIRefresh:)
+                   name:kLGPrefsUIRefreshNotification
+                 object:nil];
+    [center addObserver:target
+               selector:@selector(handleRespringStateChanged:)
+                   name:kLGPrefsRespringChangedNotification
+                 object:nil];
+}
+
+NSString *LGLocalized(NSString *key) {
+    NSBundle *bundle = LGActiveLocalizationBundle();
+    NSString *localized = [bundle localizedStringForKey:key value:key table:nil];
+    if (localized.length && ![localized isEqualToString:key]) return localized;
+    NSBundle *baseBundle = [NSBundle bundleForClass:[LGPRootListController class]];
+    return [baseBundle localizedStringForKey:key value:key table:nil];
+}
+
+NSString *LGPrefsAppName(void) {
+    return LGLocalized(@"prefs.app_name");
 }
 
 NSString *LGCurrentPrefsLanguageCode(void) {
