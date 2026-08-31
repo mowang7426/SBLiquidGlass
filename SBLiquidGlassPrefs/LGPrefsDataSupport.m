acaea41b9168b95a1cf5d62720a8c2c22295b005
mowang7426 <972565726@qq.com>
2026-09-01 04:00:47 +0800

    Add files via upload

--- SBLiquidGlassPrefs/LGPrefsDataSupport.m
+++ SBLiquidGlassPrefs/LGPrefsDataSupport.m
@@ -66,56 +66,6 @@ static NSBundle *LGActiveLocalizationBundle(void) {
     return localizedBundle ?: baseBundle;
 }
 
-static NSString *LGDisplayNameForLanguageCode(NSString *languageCode) {
-    if (!languageCode.length) return @"";
-    if ([languageCode isEqualToString:@"en"]) return @"English";
-
-    NSLocale *displayLocale = [NSLocale currentLocale];
-    NSString *localeIdentifier = [NSLocale canonicalLocaleIdentifierFromString:languageCode];
-    NSString *name = [displayLocale displayNameForKey:NSLocaleIdentifier value:localeIdentifier];
-    if (!name.length) {
-        NSDictionary *components = [NSLocale componentsFromLocaleIdentifier:localeIdentifier];
-        NSString *baseLanguageCode = components[NSLocaleLanguageCode];
-        if (baseLanguageCode.length) {
-            name = [displayLocale localizedStringForLanguageCode:baseLanguageCode];
-        }
-    }
-    return name.length ? name : languageCode;
-}
-
-static NSArray<NSDictionary *> *LGAvailableLanguageChoices(void) {
-    static NSArray<NSDictionary *> *choices;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        NSBundle *baseBundle = [NSBundle bundleForClass:[LGPRootListController class]];
-        NSMutableOrderedSet<NSString *> *codes = [NSMutableOrderedSet orderedSetWithObject:@"en"];
-        for (NSString *path in [baseBundle pathsForResourcesOfType:@"lproj" inDirectory:nil]) {
-            NSString *languageCode = [[path lastPathComponent] stringByDeletingPathExtension];
-            if (languageCode.length && ![languageCode isEqualToString:@"Base"]) {
-                [codes addObject:languageCode];
-            }
-        }
-
-        NSMutableArray<NSDictionary *> *dynamicChoices = [NSMutableArray arrayWithCapacity:codes.count];
-        for (NSString *languageCode in codes) {
-            [dynamicChoices addObject:@{
-                @"value": languageCode,
-                @"title": LGDisplayNameForLanguageCode(languageCode)
-            }];
-        }
-
-        [dynamicChoices sortUsingComparator:^NSComparisonResult(NSDictionary *lhs, NSDictionary *rhs) {
-            NSString *leftValue = lhs[@"value"];
-            NSString *rightValue = rhs[@"value"];
-            if ([leftValue isEqualToString:@"en"]) return NSOrderedAscending;
-            if ([rightValue isEqualToString:@"en"]) return NSOrderedDescending;
-            return [lhs[@"title"] localizedCaseInsensitiveCompare:rhs[@"title"]];
-        }];
-        choices = [dynamicChoices copy];
-    });
-    return choices;
-}
-
 Class LGPrefsSwitchClass(void) {
     return NSClassFromString(@"LGPrefsLiquidSwitch") ?: [UISwitch class];
 }
