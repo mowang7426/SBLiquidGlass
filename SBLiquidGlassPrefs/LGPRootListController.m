3150482818418736bf7fcb94cbdece4d7513ee84
mowang7426 <972565726@qq.com>
2026-09-01 12:05:15 +0800

    Add files via upload

--- SBLiquidGlassPrefs/LGPRootListController.m
+++ SBLiquidGlassPrefs/LGPRootListController.m
@@ -969,6 +969,11 @@ static NSString * const kLGRuntimeCacheUsageBytesKey = @"__runtime_cache_usage_b
 - (void)buildSettingsPage {
     [self installPage:&_lg_settingsScroll stack:&_lg_settingsStack];
 
+    // 详情设置：玻璃参数 + 锁屏时间详细设置
+    [_lg_settingsStack addArrangedSubview:[self lgSectionViewWithTitle:@"详情设置" subtitle:@"玻璃参数与锁屏时间详细设置"]];
+    [_lg_settingsStack addArrangedSubview:[self lgGroupedCardForItems:LGGlobalGlassTuningItems()]];
+    [_lg_settingsStack addArrangedSubview:[self lgGroupedCardForItems:LGClockItems()]];
+
     [_lg_settingsStack addArrangedSubview:[self lgSectionViewWithTitle:LGLocalized(@"prefs.settings.appearance.title") subtitle:nil]];
     [_lg_settingsStack addArrangedSubview:[self lgGroupedCardForItems:LGAppearanceSettingsItems()]];
 
